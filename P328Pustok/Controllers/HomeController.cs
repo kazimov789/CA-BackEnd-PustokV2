d225de08cf0a2113524ae146c6752e39776e56a5
kazimov789 <118839541+kazimov789@users.noreply.github.com>
2023-05-08 08:33:29 +0400

    Pustok v2

---
+++ P328Pustok/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P328Pustok.DAL;
+using P328Pustok.Models;
+using P328Pustok.ViewModels;
+using System.Diagnostics;
+
+namespace P328Pustok.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly PustokContext _context;
+
+        public HomeController(PustokContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            HomeViewModel vm = new HomeViewModel
+            {
+                FeaturedBoooks = _context.Books.Include(x=>x.Author).Include(x=>x.BookImages).Where(x => x.IsFeatured).Take(10).ToList(),
+                NewBoooks = _context.Books.Include("Author").Include(x => x.BookImages).Where(x => x.IsNew).Take(10).ToList(),
+                DiscountedBoooks = _context.Books.Include(x=>x.Author).Include(x => x.BookImages).Where(x => x.DiscountPercent>0).Take(10).ToList(),
+                Sliders = _context.Sliders.ToList()
+                //bookımage = _context.bookımages.ınclude(x => x.ımages.ımgurl).tolist()
+                //BookImage = _context.Images.ToList()
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ P328Pustok/wwwroot/js/main.js
@@ -0,0 +1,25 @@
+﻿$(document).on("click", ".modal-btn", function (e) {
+    e.preventDefault();
+
+    let url = $(this).attr("href");
+
+    fetch(url)
+        .then(response => {
+
+            console.log(response)
+            if (response.ok) {
+                return response.text()
+            }
+            else {
+                alert("Bilinmedik bir xeta bas verdi!")
+            }
+        })
+        .then(data => {
+            //$("#quickModal .product-title").text(data.book.name)
+            $("#quickModal .modal-dialog").html(data)
+            $("#quickModal").modal('show');
+        })
+    //get date
+    //data set to modal
+    //modal show
+})
